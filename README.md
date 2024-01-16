00acbd27361d3b0eb6f1d017484f51d3b7f2e57d
the.sholaakanbi <oluwalonshola6@gmail.com>
2024-01-16 08:39:34 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,73 @@
+## The title of my project
+Team Profile Generator
+---
+## Description
+The Team Profile Generator is a command-line application built with Node.js that allows managers to generate a webpage displaying information about their software engineering team. The application prompts the user to enter details about the team members, including the manager, engineers, and interns. It then generates an HTML webpage with summaries for each team member.
+---
+
+## User Story
+As a manager, I want to generate a webpage that displays my team's basic info so that I have quick access to their emails and GitHub profiles.
+
+Table of Contents
+Installation
+Usage
+Tests
+Contributing
+Questions
+License
+Installation
+To install the Team Profile Generator, follow these steps:
+---
+
+
+## Installation
+To install the Team Profile Generator, follow these steps:
+
+1. Clone the repository to your local machine. https://github.com/d-sholaakanbi/Team-profile-Generator.git
+2. Navigate to the project's directory. "cd team-profile-generator"
+3. Install the required dependencies. "npm install"
+---
+
+## Deployed Site
+
+
+## Usage
+To use the Team Profile Generator, follow these steps:
+1. Open your terminal and navigate to the project's directory.
+2. Run the application using the following command: "node index.js"
+3. Answer the prompted questions to provide information about the team members.
+4. Choose to add an engineer, add an intern, or finish building the team.
+5. Once all team members are added, the application will generate an HTML file in the output folder.
+
+---
+
+## Tests
+To run tests for the Team Profile Generator, use the following command:
+`bash
+npm test
+
+---
+
+## Contributing
+Contributions are welcome! If you would like to contribute to this project, please follow these steps:
+
+Fork the repository.
+Create a new branch for your feature or enhancement.
+Make your changes and commit them with descriptive commit messages.
+Push your changes to your fork.
+Submit a pull request to the main repository.
+
+
+--- 
+## Questions
+If you have any questions or need further assistance, feel free to reach out:
+
+GitHub: D-sholaakanbi
+Email: oluwalonshola6@gmail.com
+
+---
+## License
+This project is licensed under the MIT License - see the LICENSE file for details.
+
+---
+
---
+++ index.js
@@ -0,0 +1,129 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./src/page-template.js");
+
+// TODO: Write Code to gather information about the development team members, and render the HTML file.
+
+// Array for answers to questions
+const newStaffMemberData = [];
+
+// Array object questions asked in CLI to user
+const questions = async () => {
+  const answers = await inquirer.prompt([
+    {
+      type: "input",
+      message: "What is your name?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What is your ID number?",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "What is your email?",
+      name: "email",
+    },
+    {
+      type: "list",
+      message: "What is your role?",
+      name: "role",
+      choices: ["Engineer", "Intern", "Manager"],
+    },
+  ]);
+
+
+  // if statement for the manager, Engineer and Intern selected, answer these specific question
+
+  if (answers.role === "Manager") {
+    const managerAns = await inquirer.prompt([
+      {
+        type: "input",
+        message: "What is your office number",
+        name: "officeNumber",
+      },
+    ]);
+    const newManager = new Manager(
+      answers.name,
+      answers.id,
+      answers.email,
+      managerAns.officeNumber
+    );
+    newStaffMemberData.push(newManager);
+  } else if (answers.role === "Engineer") {
+    const githubAns = await inquirer.prompt([
+      {
+        type: "input",
+        message: "What is your GitHub user name?",
+        name: "github",
+      },
+    ]);
+    const newEngineer = new Engineer(
+      answers.name,
+      answers.id,
+      answers.email,
+      githubAns.github
+    );
+    newStaffMemberData.push(newEngineer);
+  } else if (answers.role === "Intern") {
+    const internAns = await inquirer.prompt([
+      {
+        type: "input",
+        message: "What university did you attend?",
+        name: "school",
+      },
+    ]);
+
+    const newIntern = new Intern(
+      answers.name,
+      answers.id,
+      answers.email,
+      internAns.school
+    );
+    newStaffMemberData.push(newIntern);
+  }
+};
+
+// so far looks good to me. Now the prompt questions are needed to add.
+
+async function promptQuestions() {
+    await questions()
+      
+    
+    const addMemberAns = await inquirer
+      .prompt([
+        {
+          name:'addMember',
+          type: 'list',
+          choices: ['Add a new member', 'Create team'],
+          message: "What would you like to do next?"
+        }
+      ])
+  
+      if (addMemberAns.addMember === 'Add a new member') {
+        return promptQuestions()
+      }
+      return createTeam();
+  }  
+  
+  promptQuestions();
+  
+  function createTeam () {
+    console.log("new guy", newStaffMemberData)
+    fs.writeFileSync(
+      "./output/index.html",
+      generateTeam(newStaffMemberData),
+      "utf-8"
+    );
+  }
+
+//   completed
